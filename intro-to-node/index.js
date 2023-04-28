83132ca2eddcf130e7250245a0cbefb8911d9d9d
Tharaka-98 <128624191+Tharaka-98@users.noreply.github.com>
2023-04-28 11:09:40 +0000

    Initial commit

---
+++ intro-to-node/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const App = express();
+App.use(bodyParser.urlencoded({ extended: true }));
+
+App.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+App.get("/about", (req, res) => res.sendFile("Hi welcome to devtown "));
+
+App.post("/", function(req, res) {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = num1 + num2;
+  res.send("The reult of your calculation is " + result);
+});
+
+App.listen(8000, function() {
+  console.log("The server started on the port 8000");
+});
+
